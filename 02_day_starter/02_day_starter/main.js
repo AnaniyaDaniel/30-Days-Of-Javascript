743ff40eaab9ef33aeb0324d8e7ce9303252ef7c
Ananiya Daniel <ananiyadaniel670@gmail.com>
2026-01-27 16:21:25 -0600

    02_day_Datatypes

---
+++ 02_day_starter/02_day_starter/main.js
@@ -0,0 +1,62 @@
+// A JavaScript variable can hold 8 types of data.
+// 7 Primitive data types and 1 object data type
+// type of tell us the type of the value
+
+// Number
+let meter = 16;
+let weight = 7.5;
+console.log(meter);
+console.log(weight);
+console.log(typeof meter);
+
+// BigInt
+let a = 1234567890123456789012345n;
+let b = BigInt(1234567890123456789012345);
+console.log(a);
+console.log(b);
+console.log(typeof b);
+
+// Strings
+let color = "Yellow";
+let word = "hello";
+console.log(color);
+console.log(word);
+console.log(typeof word);
+
+// Boolean
+let c = true;
+let d = false;
+console.log(c);
+console.log(d);
+console.log(typeof d);
+
+// Undefined
+let e;
+let f;
+console.log(e);
+console.log(f);
+console.log(typeof e);
+
+// Null
+let g = null;
+let h = null;
+console.log(g);
+console.log(h);
+console.log(typeof g);
+
+// Symbol
+const i = Symbol();
+const j = Symbol();
+console.log(i);
+console.log(j);
+console.log(typeof j);
+
+// Object
+const person = { firstName: "Ananiya", lastName: "Daniel" };
+console.log(person);
+console.log(typeof person);
+
+// Array Object
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+console.log(typeof cars);
